a329cb03bd210a7441fc41ff846d1aee7ead076d
emanuelhardwell <emanuelnoe16@gmail.com>
2022-04-27 19:11:27 -0500

    usando el HOOK useEffect con su CleanUp para DESMONTAR eventos

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,10 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  const { x, y } = coords;
+
   useEffect(() => {
-    console.log("Componente Montado");
+    console.log("Componente MONTADO");
+
+    const mouseMove = (e) => {
+      setCoords({ x: e.x, y: e.y });
+      console.log(":D");
+    };
+
+    window.addEventListener("mousemove", mouseMove);
 
     return () => {
+      window.removeEventListener("mousemove", mouseMove);
       console.log("Componente Desmontado");
     };
   }, []);
@@ -13,6 +25,8 @@ export const Message = () => {
   return (
     <>
       <h1 className="text-primary">Eres super genial</h1>
+      <p>x: {x}</p>
+      <p>y: {y}</p>
     </>
   );
 };
