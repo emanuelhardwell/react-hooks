d470c53092eb42baece45a2ba5e9f1475e403e22
emanuelhardwell <emanuelnoe16@gmail.com>
2022-04-30 14:59:00 -0500

    Integrando Context a nuestro proyecto

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
 
 export const LoginScreen = () => {
+  const userContext = useContext(UserContext);
+  console.log(userContext);
+
   return (
     <div>
       <h1>LoginScreen</h1>
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { AppRouter } from "./AppRouter";
+import { UserContext } from "./UserContext";
+
+const user = {
+  id: "1234",
+  name: "Emanuel",
+  email: "emanuel@gmail.com",
+};
 
 export const MainApp = () => {
   return (
     <>
-      <AppRouter />
+      <UserContext.Provider value={user}>
+        <AppRouter />
+      </UserContext.Provider>
     </>
   );
 };
